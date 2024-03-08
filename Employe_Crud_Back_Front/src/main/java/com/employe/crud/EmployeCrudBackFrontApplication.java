d4c0e14353fda84d2887b1ed8a815fa5aa737343
OmiJ80 <121397278+OmiJ80@users.noreply.github.com>
2024-03-08 14:35:46 +0530

    Add files via upload

---
+++ Employe_Crud_Back_Front/src/main/java/com/employe/crud/EmployeCrudBackFrontApplication.java
@@ -0,0 +1,13 @@
+package com.employe.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeCrudBackFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeCrudBackFrontApplication.class, args);
+	}
+
+}
